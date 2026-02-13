7f048c3e1413c579292504f2a554bec54efd2bbb
Erick Matias Granillo Mejia <230045utxicotepec.edu.mx>
2026-02-13 10:30:24 -0600

    cambios

--- models/user.py
+++ models/user.py
@@ -7,15 +7,14 @@ class Usuario(Base):
     __tablename__ = "c_usuario"
 
     Id = Column(Integer, primary_key=True, index=True)
-    ro_Id = Column(Integer, ForeignKey("c_rol.id"))
+    rol_Id = Column(Integer, ForeignKey("c_rol.id"))
     nombre = Column(String(60))
-    apellidoPaterno = Column(String(60))
-    apellidoMaterno = Column(String(60))
+    primer_apellido = Column(String(60))
+    segundo_apellido = Column(String(60))
     direccion = Column(String(200))
     correo_electronico= Column(String(100))
     numero_telefono= Column(String(20))
-    password = Column(String(256))
-
-    estatus= Column(Boolean)
+    contrasena = Column(String(256))
+    estado= Column(Boolean)
     fecha_registro = Column(DateTime)
     fecha_actualizacion = Column(DateTime)
